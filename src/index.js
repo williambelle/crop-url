515a188c29a44589fe8fbcd27b304b2bc51c312c
William Belle <william.belle@gmail.com>
2017-02-23 19:46:38 +0100

    check input type

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
 'use strict';
+
+module.exports = function(url, length) {
+
+  if (typeof url !== 'string') {
+    throw new TypeError('Expected url to be a string');
+  }
+
+  if (typeof length !== 'number') {
+    throw new TypeError('Expected length to be a number');
+  }
+
+  if (url.length <= length) {
+    return url;
+  }
+
+};
--- test/test-crop-url.js
+++ test/test-crop-url.js
@@ -1,1 +1,22 @@
 'use strict';
+
+var should  = require('chai').should();
+var cropUrl = require('../src/index.js');
+
+describe('cropUrl', function(){
+  it('should check input type', function(){
+    (function(){
+      cropUrl(51,69);
+    }).should.throw(TypeError, 'Expected url to be a string');
+    (function(){
+      cropUrl('foobar',69);
+    }).should.not.throw(TypeError, 'Expected url to be a string');
+
+    (function(){
+      cropUrl('foobar', undefined);
+    }).should.throw(TypeError, 'Expected length to be a number');
+    (function(){
+      cropUrl('foobar', 69);
+    }).should.not.throw(TypeError, 'Expected length to be a number');
+  });
+});
